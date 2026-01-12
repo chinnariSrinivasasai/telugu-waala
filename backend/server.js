20299a73923eb1b688e1b98a62cd9241ff42747b
srinivasasai chinnari <chinnarichekravarthi@gmail.com>
2026-01-12 16:31:35 +0530

    Fix streak claim crash

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,6 @@ import scratchRoutes from "./routes/scratch.routes.js";
 import withdrawRoutes from "./routes/withdraw.routes.js";
 import transactionRoutes from "./routes/transaction.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
-import streakRoutes from "./routes/streak.routes.js";
-app.use("/api/streak", streakRoutes);
-
 
 dotenv.config();
 
